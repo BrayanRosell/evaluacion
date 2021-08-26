ed5d497ac112522a1247255442ebf6bd9b0a0c87
BrayanRosell <brarosell56@gmail.com>
2021-08-26 08:09:50 -0500

    VERSION1

--- Assets/codigo/playercontroller.cs
+++ Assets/codigo/playercontroller.cs
@@ -7,7 +7,9 @@ public class playercontroller : MonoBehaviour
     private SpriteRenderer sr;
     private Animator animator;
     private Rigidbody2D rb2d;
-    private float speed = 15;
+    private float speed = 6;
+    private float speedRun = 20;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,28 +22,80 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         quieto();
+        rb2d.gravityScale = 35;
+         Quieto();
+        
         if(Input.GetKey(KeyCode.RightArrow))
         {
             sr.flipX = false;
-                     
-            rb2d.velocity = new Vector2(speed,rb2d.velocity.y);
             Correr();
+            
+            rb2d.velocity = new Vector2(speed,rb2d.velocity.y);
+            if(Input.GetKey(KeyCode.Space))
+            {            
+            saltarF();
+            
+            }
+            if(Input.GetKey(KeyCode.X))
+            {
+            Correr();
+             rb2d.velocity = new Vector2(speedRun,rb2d.velocity.y);
+            }
+             if(Input.GetKey(KeyCode.Z))
+            {
+            atakar();
+            }
+          
         }else if(Input.GetKey(KeyCode.LeftArrow))
         {
             sr.flipX = true;
-           
-            rb2d.velocity = new Vector2(-speed,rb2d.velocity.y);
             Correr();
+            rb2d.velocity = new Vector2(-speed,rb2d.velocity.y);
+            if(Input.GetKey(KeyCode.Space))
+            {
+               saltarF();
+             
+           
+            }
+            if(Input.GetKey(KeyCode.X))
+            {
+            Correr();
+             rb2d.velocity = new Vector2(-speedRun,rb2d.velocity.y);
+            }
+             if(Input.GetKey(KeyCode.Z))
+            {
+            atakar();
+            }
+        }
+        else
+        {
+            Quieto();
+            rb2d.velocity = new Vector2(0,rb2d.velocity.y);
+            if(Input.GetKey(KeyCode.Space))
+            {
+            saltarF();
+            }
+             if(Input.GetKey(KeyCode.Z))
+            {
+            atakar();
+            }
         }
     }
-    public void quieto(){
+    public void saltarF(){
+        var upSpeed = 80f;
+        rb2d.velocity = Vector2.up * upSpeed;
+        saltar();
+    }
+    public void Quieto(){
         animator.SetInteger("estado", 0);        
     }
      public void Correr(){
         animator.SetInteger("estado", 1);        
     }
-     public void muere(){
+     public void saltar(){
         animator.SetInteger("estado", 2);        
+    }
+    public void atakar(){
+        animator.SetInteger("estado", 3);        
     }
 }
